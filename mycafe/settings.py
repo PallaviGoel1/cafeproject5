4a3d8f4618b4599f7c862d0f84fb660ba7c502ae
Pallavi Goel <pallavigoel2610@gmail.com>
2024-02-27 09:41:03 +0000

    add static and media

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-txrx^4b99b^9g!zmtdz6uow^c04sikj^)wc%b7ow^51$z)lf7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-pallavigoel-cafeproject-yswwgpgz90a.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-pallavigoel-cafeproject-kftqelbtos1.ws-eu108.gitpod.io']
 
 
 # Application definition
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
