ff5f490167966ef6674e087a066a3adbfafcb169
Pallavi Goel <pallavigoel2610@gmail.com>
2024-03-17 09:31:47 +0000

    add sizes,quantity item with update and remove

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-txrx^4b99b^9g!zmtdz6uow^c04sikj^)wc%b7ow^51$z)lf7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-pallavigoel-cafeproject-7129tmu82xl.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-pallavigoel-cafeproject-i5iah279mux.ws-eu110.gitpod.io']
 
 
 # Application definition
