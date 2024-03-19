403d1b91a40bcabc29b47b5f7a4d4ecc5b84d5c0
Pallavi Goel <pallavigoel2610@gmail.com>
2024-03-19 12:32:48 +0000

    add class webhook handler,view and import json

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["STRIPE_PUBLIC_KEY"]= "pk_test_51OvjsnCUt5xn2cAuQHcccv2UnM6OlADvyavsYJHQQ5CJKxD1z55AU06uH5h6uxSfaI1nsZAsI791KzGpDyB3G8bp00nz63byme"
+os.environ["STRIPE_SECRET_KEY"]= "sk_test_51OvjsnCUt5xn2cAuGrFj4u0TL6832GbnY0Y3T7VDMEYEv4zgnlmmeFV3YvfrMQ1lr53F4jCYUdSZEFMRjP8GuQTz00G6xixnT1"
+os.environ["STRIPE_WH_SECRET"] = "whsec_DhGZBuTtPLNNaQbwmX0FOSourOzAAfhp"
