fb5b7d1eef51221a65bd49dcf2564e2963c73b6b
Pallavi Goel <pallavigoel2610@gmail.com>
2024-03-24 09:55:39 +0000

    deploy p2

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -204,8 +204,8 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'coffeehouse'
-    AWS_S3_REGION_NAME = 'Europe'
+    AWS_STORAGE_BUCKET_NAME = 'coffeehousecafe'
+    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
