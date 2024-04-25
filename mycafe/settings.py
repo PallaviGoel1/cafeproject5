a2bf3bdf3ecc31bdd3c75b1714f4fbb8219d2b76
Pallavi Goel <pallavigoel2610@gmail.com>
2024-04-25 20:04:49 +0000

    add correct image url in admin django

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -20,15 +20,16 @@ if os.path.exists("env.py"):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-#SECRET_KEY = '4yt1)f9z(b(qi-(cvt9+ybvazu_6$tg%=*0pyfd%67e-vnguj$'
+#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '4yt1)f9z(b(qi-(cvt9+ybvazu_6$tg%=*0pyfd%67e-vnguj$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
