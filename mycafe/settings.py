7950930c1a7014a3705ed3ce300f4e16ca8e6794
Pallavi Goel <pallavigoel2610@gmail.com>
2024-05-05 12:16:09 +0000

    Add allowed hosts path in setting.py

--- mycafe/settings.py
+++ mycafe/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = [
     'coffeehouse.herokuapp.com', 
     'localhost',
     'coffeehouse-fee0be3a9012.herokuapp.com',
-    '8000-pallavigoel-cafeproject-piab2k4ilf6.ws-eu110.gitpod.io'
+    '8000-pallavigoel-cafeproject-ryz3qu54ere.ws-eu111.gitpod.io'
 ]
 
 # Application definition
